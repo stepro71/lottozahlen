e2d99265b1ca73b9987bce1b81d595c10fbafbde
stepro71 <32392324+stepro71@users.noreply.github.com>
2020-12-22 18:56:21 +0100

    Add files via upload

---
+++ start.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from math import comb
+import random
+with st.echo():
+    st.title("Lottozahlen Generator")
+    x = st.slider('Anzahl der Zahlen',max_value=100)
+    y = st.slider('Wieviele Zahlen kann man setzen?',max_value=100)
+    st.write( 'Anzahl der Möglichkeiten', comb(x,y))
+    z = st.slider('Wieviele Tips?',max_value=20)
+
+
+    st.header("Erzeugte Tips")
+#list2 = [list1.pop(list1.index(min(list1))) for i in range(len(list1))]
+    for i in range (0,z):
+        list1 = sorted(random.sample(range(1,x), y))
+        list2 = ' '.join(map(str, list1)) 
+        st.write("" , list2)
