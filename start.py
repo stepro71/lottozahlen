27f9ec31b8125353cf447b0364fcd7cbc0b37164
stepro71 <32392324+stepro71@users.noreply.github.com>
2020-12-23 17:17:12 +0100

    Update start.py

--- start.py
+++ start.py
@@ -3,14 +3,14 @@ from scipy.special import comb
 import random
 
 st.title("Lottozahlen Generator")
+st.write("Bis auf uninteressante Imports (3 Zeilen) ist das der ganze Code")
+st.header("Generator")
 with st.echo():
     x = st.slider('Anzahl der Zahlen',max_value=100)
     y = st.slider('Wieviele Zahlen kann man setzen?',max_value=100)
     st.write( 'Anzahl der Möglichkeiten', comb(x,y))
     z = st.slider('Wieviele Tips?',max_value=20)
-
     st.header("Erzeugte Tips")
-#list2 = [list1.pop(list1.index(min(list1))) for i in range(len(list1))]
     for i in range (0,z):
         list1 = sorted(random.sample(range(1,x), y))
         list2 = ' '.join(map(str, list1)) 
