9b87228489e2cbd3edb9bf5a9aa425497f4563b8
stepro71 <32392324+stepro71@users.noreply.github.com>
2020-12-23 17:25:00 +0100

    Update start.py

--- start.py
+++ start.py
@@ -9,8 +9,6 @@ with st.echo():
     #import random
 
     st.title("Lottozahlen Generator")
-    st.write("Folgende Imports verwenden:")
-    st.header("Generator")
     x = st.slider('Anzahl der Zahlen',max_value=100)
     y = st.slider('Wieviele Zahlen kann man setzen?',max_value=100)
     st.write( 'Anzahl der Möglichkeiten', comb(x,y))
